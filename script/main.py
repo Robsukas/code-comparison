ab0b91c72e676ea0d0b4856033d50f1cf91fc590
Robin Nook <robin.nook4@gmail.com>
2025-02-10 02:00:29 +0200

    valmis algne versioon kahe koodi võrdlusfunktsionaalsusest

--- script/main.py
+++ script/main.py
@@ -1,9 +1,10 @@
 import textwrap
 from AST_parser import ASTParser
 from AST_normalizer import ASTNormalizer
+from AST_comparator import ASTComparator
 
 if __name__ == "__main__":
-    sample_code = textwrap.dedent("""
+    student_code = textwrap.dedent("""
     def clock(päevad: int, tunnid: int, minutid: int, sekundid: int):
         minutes = 0
         minutes += päevad * 1440
@@ -13,10 +14,31 @@ if __name__ == "__main__":
         return minutes
     """)
 
+    teacher_code = textwrap.dedent("""
+    def clock_time(päevad: int, tunnid: int, minutid: int, sekundid: int):
+        minutes = 0
+        minutes += sekundid / 60
+        minutes += minutid
+        minutes += tunnid * 60
+        minutes += päevad * 1440
+        return minutes
+    """)
+
     parser = ASTParser()
     normalizer = ASTNormalizer()
+    comparator = ASTComparator()
 
-    tree_dict = parser.parse_code(sample_code)
-    normalized_tree_dict = normalizer.normalize_ast_dict(tree_dict)
+    student_ast = parser.parse_code(student_code)
+    teacher_ast = parser.parse_code(teacher_code)
 
-    print("Parsed AST dictionary:\n", normalized_tree_dict)
+    student_norm, _, _ = normalizer.normalize_ast_dict(student_ast)
+    teacher_norm, _, _ = normalizer.normalize_ast_dict(teacher_ast)
+
+    differences = comparator.compare_ast_dicts(student_norm, teacher_norm)
+
+    if differences:
+        print("Differences found:")
+        for diff in differences:
+            print(diff)
+    else:
+        print("No differences found!")
